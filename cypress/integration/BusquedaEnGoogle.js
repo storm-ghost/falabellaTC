86137d7688c294da7fe996ecbad9dc2f8275ccca
storm-ghost <cristian.rubilar.f@gmail.com>
2022-01-19 09:53:51 -0300

    Actualización de código
    actualización de codigo

--- cypress/integration/BusquedaEnGoogle.js
+++ cypress/integration/BusquedaEnGoogle.js
@@ -1,9 +1,17 @@
 describe('FalabellaTC',() => {
-it('Busqueda en Google',() => {
-    cy.visit('https://www.google.cl/');
-    cy.get('.gLFyf').type('falabella{enter}');
-    cy.contains('Falabella.com | Todo lo que necesitas en un solo lugar')
-    .click();
-
+it('Ingresar En Google',() => {
+    cy.visit('https://www.google.cl/')
 })
+
+it('Buscar En Google',() => {
+    cy.get('input[name="q"]').type('falabella.com{enter}')
+    //cy.get('#search a').invoke('attr','href')
+    //cy.contains('Falabella.com | Todo lo que necesitas en un solo lugar').click();
+})
+
+it('Ingresar a resultado de busqueda de falabella.com',() => {
+    //cy.get('#search a').invoke('attr','href')
+    //cy.contains('Falabella.com | Todo lo que necesitas en un solo lugar').click();
+})
+
 })
--- cypress/integration/FalabellaTC.js
+++ cypress/integration/FalabellaTC.js
@@ -1,20 +1,49 @@
 describe('FalabellaTC',() => {
-    it('Busqueda XBOX',() => {
+    
+    it('Ingresar a falabella.com',() => {
         cy.visit('https://www.falabella.com/falabella-cl')
-        cy.wait(15000)
+    })
+    
+    it('Cerrar POP-UP',() => {
         cy.log('Existe POP-UP')
         cy.get('.dy-lb-close').should('exist')
         cy.get('.dy-lb-close').click()
-        
-        cy.get('#testId-SearchBar-Input').type('Consola XBOX Series')
-        cy.get('.SearchBar-module_searchBtnIcon__2L2s0').click()
-        cy.get('#buttonForCustomers').click()
-        
-        
-
-        //cy.get('.gLFyf').type('falabella{enter}');
-        //cy.contains('Falabella.com | Todo lo que necesitas en un solo lugar')
-        //.click();
-
     })
+
+
+    it('Busqueda XBOX',() => {        
+        cy.get('#testId-SearchBar-Input').type('Consola XBOX Series')
+        cy.get('.SearchBar-module_searchBtnIcon__2L2s0').click()    
+    })
+
+    it('Agregar producto al carro',() => {
+        cy.contains('Agregar al Carro').click({force: true})
+    })
+
+    it('Valida Modal Carrito de compras',() => {
+        cy.contains('Lo que llevas en tu Carro').should('exist')
+    })
+    
+    it('Agregar Garantia x 2 años',() => {
+        cy.contains('Consolas 2 Años').click()
+    })
+
+    it('Ir al carro de compras',() => {
+        cy.contains('Ir al Carro').click()
+    })
+
+    it('Agregar producto al carro',() => {
+        cy.get('.jsx-1566303277.increment').click()
+        //cy.contains('Ir al Carro').click({force: true})
+    })
+
+    it('Continuar Compra',() => {
+        cy.contains('Continuar compra').click()
+    })
+    
+    it('Valida que se avanza al checkout de la compra',() => {
+        cy.contains('Resumen de la compra').should('exist')
+    })
+    
+
 })
